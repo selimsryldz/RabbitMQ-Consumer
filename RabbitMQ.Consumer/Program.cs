271c1d7cb15192ebda2fc180d05b82a64a12cb00
Mehmet Selim Sarıyıldız <mehmetselim.sariyildiz@wy.com.tr>
2022-07-13 02:24:14 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:13579630@localhost:5672") };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            DirectExchangeConsumer.Consume(channel);
+
+        }
+    }
+}
